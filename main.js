ffa57460091a3f6140cb7c8081e2188cc1d33e60
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-03 05:30:23 -0700

    add file

--- main.js
+++ main.js
@@ -1,3 +1,76 @@
+// const productItemElement= document.querySelectorAll(".products_item")
+
+// const productButtonElements=document.querySelectorAll(".products_item button")
+
+// productButtonElements.forEach(element =>{
+//     element.addEventListener("click", event =>{
+//         productItemElement.forEach(e =>e.classList.remove(".active"))
+//        if (event.target.className= "boots_button"){
+//            event.path[1].classList.add("active")
+//        }
+//        else{
+//         event.path[2].classList.add("active")
+//        }
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const productItemElements = document.querySelectorAll(".products_item")
 
 const productButtonElements = document.querySelectorAll(".products_item button")
