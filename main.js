b99f116a9cd6f6bcae4e4a03c311ce9bdae1c6aa
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-03 03:52:48 -0700

    add file

---
+++ main.js
@@ -0,0 +1,21 @@
+const productItemElements = document.querySelectorAll(".products_item")
+
+const productButtonElements = document.querySelectorAll(".products_item button")
+
+productButtonElements.forEach(element => {
+    element.addEventListener('click', event => {
+        const activeElements = document.querySelectorAll('.active')
+        activeElements.forEach(e => {
+            e.classList.remove('active')
+            e.classList.add('sekin')
+            setTimeout(() => {
+                e.classList.remove('sekin')
+            }, 1000)
+        })
+        if(event.target.className == "boots_button"){
+            event.path[1].classList.add('active')
+        } else {
+            event.path[2].classList.add('active')
+        }
+    })
+})
