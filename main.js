b39e62ac5a3907c8f94a96015cb501f0fee39421
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-04 03:56:54 -0700

    add file

--- main.js
+++ main.js
@@ -75,6 +75,12 @@ const productItemElements = document.querySelectorAll(".products_item")
 
 const productButtonElements = document.querySelectorAll(".products_item button")
 
+const firstButtonElement=document.querySelector("#first")
+const secondButtonElement=document.querySelector("#second")
+const threeButtonElement=document.querySelector("#third")
+const fourthButtonElement=document.querySelector("#fourth")
+
+
 productButtonElements.forEach(element => {
     element.addEventListener('click', event => {
         const activeElements = document.querySelectorAll('.active')
@@ -91,4 +97,60 @@ productButtonElements.forEach(element => {
             event.path[2].classList.add('active')
         }
     })
+})
+
+firstButtonElement.addEventListener("click",event =>{
+    const backImageELement=document.querySelector("#back")
+
+    const backPictureElement=document.querySelector(".back-image")
+    
+    backImageELement.style.backgroundImage='url("./assets/img/6.jpg")'
+    backImageELement.style.width="320px"
+    backImageELement.style.height="400px"
+    backImageELement.style.backgroundSize="contain"
+    backImageELement.style.backgroundRepeat="no-repeat"
+    backPictureElement.style.width='0'
+})
+
+secondButtonElement.addEventListener("click",event =>{
+    const backImageELement=document.querySelector("#back")
+
+    const backPictureElement=document.querySelector(".back-image")
+    
+    backImageELement.style.backgroundImage='url("./assets/img/9.jpg")'
+    
+    backImageELement.style.backgroundSize="contain"
+    backImageELement.style.backgroundRepeat="no-repeat"
+    backImageELement.style.width="320px"
+    backImageELement.style.height="400px"
+    backPictureElement.style.width='0'
+})
+
+threeButtonElement.addEventListener("click",event =>{
+    const backImageELement=document.querySelector("#back")
+
+    const backPictureElement=document.querySelector(".back-image")
+    
+    backImageELement.style.backgroundImage='url("./assets/img/8.jpg")'
+    
+    backImageELement.style.backgroundSize="contain"
+    backImageELement.style.backgroundRepeat="no-repeat"
+    backImageELement.style.width="320px"
+    backImageELement.style.height="400px"
+    backPictureElement.style.width='0'
+})
+
+
+fourthButtonElement.addEventListener("click",event =>{
+    const backImageELement=document.querySelector("#back")
+
+    const backPictureElement=document.querySelector(".back-image")
+    
+    backImageELement.style.backgroundImage='url("./assets/img/7.jpg")'
+    
+    backImageELement.style.backgroundSize="contain"
+    backImageELement.style.backgroundRepeat="no-repeat"
+    backImageELement.style.width="320px"
+    backImageELement.style.height="400px"
+    backPictureElement.style.width='0'
 })
